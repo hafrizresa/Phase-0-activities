e4d7f9f5d534d25962fdc9ac02a5340f4e2d75a6
hafrizresa <hafrizresa@gmail.com>
2018-08-29 00:43:32 +0700

    add tugas loop asterisk

---
+++ tugas/Week-2/Exercise-7-loopwithasterisks.js
@@ -0,0 +1,85 @@
+// 1. Menyusun Barisan Bintang
+console.log("1. Menyusun Barisan Bintang")
+var rows1 = 5;
+var a = 0;
+var b = "*";
+for (a = 0; a < rows1; a++) {
+    console.log(b)
+}
+
+// ATAU
+console.log("ATAU");
+a = 0
+while (a < rows1) {
+    console.log(b)
+    a++
+}
+
+// 2. Menyusun Barisan Bintang Dengan Nested Looping
+console.log("2. Menyusun Barisan Bintang Dengan Nested Looping")
+var rows2 = 5;
+var a2 = 0;
+var b2 = 0;
+var c2 = ""
+for (a2 = 0; a2 < rows2; a2++) {
+    for (b2 = 0; b2 < rows2; b2++) {
+        c2 = c2 + "*"
+    }
+    console.log(c2);
+    c2 = ""
+}
+
+// ATAU
+console.log("ATAU")
+a2 = 0;
+b2 = 0;
+while (a2 < rows2) {
+    while (b2 < rows2) {
+        c2 = c2 + "*";
+        b2++
+    }
+    console.log(c2);
+    //   apakah loop while selalu ke reset default setiap variabel setelah selesai fungsinya? tolong jelaskan terimakasih
+    a2++
+}
+
+// 3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+console.log("3. Menyusun Barisan Tangga Bintang Dengan Nested Looping")
+var rows3 = 5
+var a3 = 0;
+var b3 = 0;
+var c3 = "*";
+for (a3 = 0; a3 < rows3; a3++) {
+    for (b3 = 0; b3 < a3; b3++) {
+        c3 = c3 + "*"
+    }
+    console.log(c3);
+    c3 = "*"
+}
+
+// ATAU
+console.log("ATAU");
+a3 = 0;
+b3 = 0;
+while (a3 < rows3) {
+    while (b3 < a3) {
+        c3 = c3 + "*";
+        b3++
+    }
+    console.log(c3);
+    //   apakah loop while selalu ke reset default setiap variabel setelah selesai fungsinya? tolong jelaskan terimakasih
+    a3++
+}
+
+// COBA CARA LAIN
+console.log("COBA CARA LAIN")
+c3 = "*"
+for (a3=0; a3<rows3; a3++){
+  if (a3 == 0){
+    console.log(c3)
+  }
+  else{
+    c3 = c3 + "*";
+    console.log(c3)
+  }
+}
