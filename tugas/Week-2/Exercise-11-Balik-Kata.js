881b5b3a7cd10b690b249ce7134bb97779c9544f
hafrizresa <hafrizresa@gmail.com>
2018-08-31 17:00:53 +0700

    benerin variabel

--- tugas/Week-2/Exercise-11-Balik-Kata.js
+++ tugas/Week-2/Exercise-11-Balik-Kata.js
@@ -1,17 +1,17 @@
 // Logic Challenge - Balik Kata
 function balikKata(kata) {
-    var a = kata.length - 1;
-    var b = ''
-      while (a >= 0) {
-        b = b + kata[a]
-        a = a - 1
-      }
-      return b
-    }
-    
-    // TEST CASES
-    console.log(balikKata('Hello World and Coders')); 
-    console.log(balikKata('John Doe')); 
-    console.log(balikKata('I am a bookworm'));
-    console.log(balikKata('Coding is my hobby'));
-    console.log(balikKata('Super')); 
+  var indexKata = kata.length - 1;
+  var hasil = ''
+  while (indexKata >= 0) {
+    hasil = hasil + kata[indexKata]
+    indexKata = indexKata - 1
+  }
+  return hasil
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders'));
+console.log(balikKata('John Doe'));
+console.log(balikKata('I am a bookworm'));
+console.log(balikKata('Coding is my hobby'));
+console.log(balikKata('Super')); 
--- tugas/Week-2/Exercise-12-Konversi-Menit.js
+++ tugas/Week-2/Exercise-12-Konversi-Menit.js
@@ -1,23 +1,23 @@
 // Logic Challenge - Konversi Menit
 function konversiMenit(menit) {
-    var b = 0;
-    while(menit%60 != 0){
-      b = b + 1;
-      menit = menit - 1
-    }
-    c = menit / 60
-    
-    if (c<10) {
-      c = '0' + c
-    }
-    if (b<10) {
-      b = '0' + b
-    }
-    return d = c + ':' + b
-    }
-    // TEST CASES
-    console.log(konversiMenit(63)); // 1:03
-    console.log(konversiMenit(124)); // 2:04
-    console.log(konversiMenit(53)); // 0:53
-    console.log(konversiMenit(88)); // 1:28
-    console.log(konversiMenit(120)); // 2:00
+  var perhitunganMenit = 0;
+  while (menit % 60 != 0) {
+    perhitunganMenit = perhitunganMenit + 1;
+    menit = menit - 1
+  }
+  var jam = menit / 60
+
+  if (jam < 10) {
+    jam = '0' + jam
+  }
+  if (perhitunganMenit < 10) {
+    perhitunganMenit = '0' + perhitunganMenit
+  }
+  return hasil = jam + ':' + perhitunganMenit
+}
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
