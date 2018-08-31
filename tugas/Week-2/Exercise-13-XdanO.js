d06c0d791b127460d42f1db77f7381d582c08cc8
hafrizresa <hafrizresa@gmail.com>
2018-08-31 17:59:52 +0700

    benerin XO tugas

--- tugas/Week-2/Exercise-13-XdanO.js
+++ tugas/Week-2/Exercise-13-XdanO.js
@@ -1,6 +1,27 @@
 // Logic Challenge - X dan O
 
 function xo(str) {
+    var countX = 0;
+    var countO = 0;
+    var counter = 0;
+
+    for (counter = 0; counter < str.length; counter++) {
+        if (str[counter] === 'x') {
+            countX = countX + 1
+        }
+        else if (str[counter] === 'o') {
+            countO = countO + 1
+        }
+    }
+    if (countX == countO) {
+        return true
+    }
+    else { return false }
+}
+
+// ATAU
+
+function xo(str) {
     var countx = str.match(/x/g);
     var indexx = countx.length;
     var counto = str.match(/o/g);
