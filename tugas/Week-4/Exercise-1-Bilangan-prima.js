3d5258163d2eb17669e95acd78d469162e48b1bd
hafrizresa <hafrizresa@gmail.com>
2018-09-11 13:02:35 +0700

    add tugas week4

---
+++ tugas/Week-4/Exercise-1-Bilangan-prima.js
@@ -0,0 +1,32 @@
+function angkaPrima(angka) {
+    var temp = 0
+    for (var i = 2; i < 10; i++) {
+        if (angka == i) {
+            temp == 0
+        }
+        else if (angka % i == 0) {
+            temp = temp + 1
+        }
+    }
+    if (temp == 0) {
+        return true
+    }
+    else { return false }
+}
+
+// ATAU
+function angkaPrima(angka){
+    for (var i=2; i<angka; i++){
+      if (angka % i == 0){
+        return false
+      }
+    }
+    return angka> 1
+  }
+  
+
+console.log(angkaPrima(3)); // true
+console.log(angkaPrima(7)); // true
+console.log(angkaPrima(6)); // false
+console.log(angkaPrima(23)); // true
+console.log(angkaPrima(33)); // false
