ee7b86806fff88597c0569d812e7f06676c46add
hafrizresa <hafrizresa@gmail.com>
2018-09-20 16:32:36 +0700

    add tugas

---
+++ tugas/Week-5/Exercise-8-cari-pelaku.js
@@ -0,0 +1,11 @@
+function cariPelaku(str) {
+    var result = str.match(/abc/g)
+    return result.length
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
