cf88388d635b6d4d9039af545c2e4dee7ecd600b
hafrizresa <hafrizresa@gmail.com>
2018-09-23 20:14:16 +0700

    resvisi tugas

--- tugas/Week-4/Exercise-4-cari-modus.js
+++ tugas/Week-4/Exercise-4-cari-modus.js
@@ -1,30 +1,32 @@
 function cariModus(arr) {
-    var tes = 0
+    var tampung = []
     for (var i = 0; i < arr.length; i++) {
-        var penampung = 0
-        var copy = arr.slice();
-        var selector = copy.splice(i, 1)
-        for (var j = 0; j < copy.length; j++) {
-            if (selector == copy[j]) {
-                penampung += 1
-                tes += 1
+        var temp = 0
+        for (var j = i; j < arr.length; j++) {
+            if (arr[i] == arr[j + 1]) {
+                temp += 1
             }
         }
-        if (penampung >= 1 && penampung != arr.length - 1) {
-            return arr[i]
-        }
-        else if (penampung == arr.length - 1) {
-            return -1
+        tampung.push(temp)
+    }
+    temp = 0
+    var angka = 0
+    for (var i = 0; i < tampung.length; i++) {
+        if (tampung[i] > temp) {
+            temp = tampung[i]
+            angka = i
         }
     }
-    if (tes == 0) {
+
+    if (temp == 0 || temp == arr.length - 1) {
         return -1
     }
+    return arr[angka]
 }
-
 
 console.log(cariModus([10, 4, 5, 2, 4])); // 4
 console.log(cariModus([5, 10, 10, 6, 5])); // 5
 console.log(cariModus([10, 3, 1, 2, 5])); // -1
 console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
 console.log(cariModus([7, 7, 7, 7, 7])); // -1
+console.log(cariModus([10, 4, 5, 2, 4, 10, 4])); // 4
