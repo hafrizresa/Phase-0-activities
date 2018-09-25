286bfcf5efeca216e5f11a211704958c3eb4d72f
hafrizresa <hafrizresa@gmail.com>
2018-09-25 12:27:26 +0700

    revisi hitung huruf

--- tugas/Week-4/Exercise-6-Hitung-Huruf.js
+++ tugas/Week-4/Exercise-6-Hitung-Huruf.js
@@ -1,25 +1,39 @@
 function hitungHuruf(kata) {
-  var arr = kata.split(' ')
-  var tempKata = '', maxIndex = 0
-
-  for (var i = 0; i < arr.length; i++) {
-    var count = 0;
-    tempKata = arr[i]
-    for (var j = 0; j < tempKata.length; j++) {
-      var tempHuruf = tempKata[j]
-
-      for (var k = j + 1; k < tempKata.length; k++) {
-        if (tempHuruf === tempKata[k]) {
-          count++
-        }
+  var splited = kata.split(' ')
+  var obj = {}
+  var temp = []
+  for (var i = 0; i < splited.length; i++) {
+    obj[splited[i]] = {}
+    for (var j = 0; j < splited[i].length; j++) {
+      if (obj[splited[i]][splited[i][j]] == undefined) {
+        obj[splited[i]][splited[i][j]] = 1
+      }
+      else {
+        obj[splited[i]][splited[i][j]] += 1
       }
     }
-    if (count > maxIndex) {
-      maxIndex = count
-      arr = tempKata
+  }
+  var selector = 0
+  var hasil;
+  for (var i in obj) {
+    var penampung = 0
+    for (var j in obj[i]) {
+      if (obj[i][j] > 1) {
+        penampung += obj[i][j]
+      }
     }
-    if (maxIndex > 0) {
-      return tempKata
+    temp.push(penampung)
+    if (selector < penampung) {
+      selector = penampung
+      hasil = i
     }
   }
+  return hasil
 }
+
+// TEST CASES
+console.log(hitungHuruf('Today, is the greatest day ever')); // greatest
+console.log(hitungHuruf('I am a passionate developer')); // passionate
+console.log(hitungHuruf('aku adalah anak gembala')); // adalah
+console.log(hitungHuruf('rajin pangkal kaya')); // pangkal
+console.log(hitungHuruf('mengayuh perahu di danau')); // danau
